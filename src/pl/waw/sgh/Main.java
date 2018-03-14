b3554bac48f0a05caf3904c5a4e2bbd29170f54e
jgdula <jakb.gdula@gmail.com>
2018-03-14 12:52:10 +0100

    Adding the first variable

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -2,7 +2,11 @@ package pl.waw.sgh;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello!!");
+
+        int a = 5;
+        System.out.println("a=" + a);
     }
 }
